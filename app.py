59cb0dbc8b8e611d81afd8e42f5795d21430e018
jeremaihloo <1006397539@qq.com>
2015-11-17 18:17:35 +1000

    vote all excel sheet and get a vote result

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from votelib import VoteHelper
 import xlrd
-
+import json
+import datetime
 
 xls_file_name = 'users.xls'
 data = xlrd.open_workbook(xls_file_name)
@@ -14,7 +15,20 @@ for table_index in range(0,9):
 
         print("[用户名：%s 密码: %s]" % (username, password))
         helper = VoteHelper()
-        helper.vote(username,password)
+        result = helper.vote(username,password)
+        data_result[str(result)]=data_result[str(result)]+1
+        print("data result %s" % data_result)
 
 print('投票结束！')
+print('成功投票：%s' % data_result['200'])
+print('投票上线：%s' % data_result['204'])
+print('重复投票：%s' % data_result['206'])
+print('登录失败：%s' % data_result['211'])
+
+fname = 'result-%s.txt' % datetime.datetime.now().strftime('%Y-%m-%d-%H-%m-%s')
+print('本次批量投票操作的结果存放在：%s' % fname)
+handler = open(fname, 'w')
+handler.write(json.dumps(data_result,sort_keys=True,indent=4))
+handler.close()
+
 input()
